a0c4ab0b070e26c864bf0e51e34ec278f469b89c
dderible <declan.derible@keyin.com>
2025-04-12 17:32:25 -0230

    initial commit

---
+++ src/main/java/com/example/BinaryTreeFS/BinaryTreeFsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BinaryTreeFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryTreeFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryTreeFsApplication.class, args);
+	}
+
+}
